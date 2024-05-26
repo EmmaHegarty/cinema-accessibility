30bcf6cacc528d8ce0d3b925f33b454b9fdc252d
Emma Hegarty <amsel.hegarty@gmail.com>
2024-05-26 12:36:56 +0200

    feat: get variables per departure time and per start point

--- scripts/analysis/calculate_variables.py
+++ scripts/analysis/calculate_variables.py
@@ -1,10 +1,12 @@
 from os import path
-from pandas import read_csv, DataFrame
+from shapely import wkt
+from pandas import read_csv, DataFrame, concat
 from geopandas import read_file, GeoDataFrame
 
 from scripts.constants import RESULTS_PATH, SELECTED
 from scripts.analysis.variables import (get_avg_time, get_speed, get_car_compare, get_transit_time, get_walk_percent,
-                                        get_avg_changes)
+                                        get_avg_changes, get_walk_time, get_max_changes, get_fastest_mode,
+                                        get_valid_entries, get_majority_mode, get_min_max_cinema)
 
 
 # calculate derived variables and write them into new columns
@@ -62,6 +64,72 @@ def write_point_layer(df, times, filename, count):
     result.to_file(path.join(RESULTS_PATH, 'geo_data', 'analysis', f'analysis_{filename}.gpkg'))
 
 
+# calculate averages for all routes at every departure time
+# {args} df: df, times: [int], filename: str
+# {returns} None, writes new file
+def get_variables_per_time(df, times, filename):
+    time_str_arr = [str(t) for t in times]
+    col = ['index'] + time_str_arr
+
+    for t in times:
+        df[f'{t}_fastest_mode'] = df.apply(
+            lambda row: 'transit' if row['foot_duration'] > row[f'{t}_total_duration'] else 'foot', axis=1)
+    new_df = DataFrame({
+        'departure times': times,
+        'average speed': [get_speed(df, t, True) for t in times],
+        'average time': [get_avg_time(df, t) for t in times],
+        'average transit time': [get_transit_time(df, t, True) for t in times],
+        'average walk time': [get_walk_time(df, t, True) for t in times],
+        'average walk share': [get_walk_percent(df, t, 'avg') for t in times],
+        'walk share under 40%': [get_walk_percent(df, t, 40) for t in times],
+        'average amount of changes': [get_avg_changes(df, t) for t in times],
+        'max changes': [get_max_changes(df, t) for t in times],
+        'time difference to car': [get_car_compare(df, t, True) for t in times],
+        'transit is reasonable': [get_fastest_mode(df, t) for t in times],
+        'out of': [get_valid_entries(df, t, True) for t in times]
+    })
+
+    new_df.to_csv(path.join(RESULTS_PATH, 'analysis', f'variables_{filename}.csv'))
+
+
+# calculate averages for all the routes from each start point
+# {args} area: df/str, times: [int], filename: str
+# {returns} None, writes new file
+def get_variables_per_start(times, filename):
+    df = read_csv(path.join(RESULTS_PATH, 'analysis', f'analysis_{filename}.csv'))
+    starts = df['start_location'].unique()
+
+    def get_variables(s):
+        sdf = df[df['start_location'] == s]
+        sdf = sdf[sdf.loc[:, 'cinema_name'::].columns]
+        cols = [item for item in sdf.columns
+                if all(c not in item for c in ['total_route', 'cinema_name', 'area', 'start_location',
+                                               'geometry', 'fastest_mode', 'fastest_overall_mode', 'level'])]
+        row = [sdf[col].mean() for col in sdf[cols].columns.values]
+        row.extend([sdf['area'].iloc[0], s])
+        row.extend(get_majority_mode(sdf, t) for t in times)
+        row.extend(get_fastest_mode(sdf, t) for t in times)
+        row.extend(sum([get_min_max_cinema(sdf, t, 'min') for t in times], []))
+        row.extend(sum([get_min_max_cinema(sdf, t, 'max') for t in times], []))
+        cols.extend(['area', 'start_location'])
+        cols.extend([f'{t} fastest mode' for t in times])
+        cols.extend([f'{t} transit is fastest' for t in times])
+        cols.extend(sum([[f'{t} min cinema dur', f'{t} most accessible cinema'] for t in times], []))
+        cols.extend(sum([[f'{t} max cinema dur', f'{t} least accessible cinema'] for t in times], []))
+        return DataFrame([row], columns=cols)
+
+    results = [get_variables(start) for start in starts]
+    save_df = concat(results)
+
+    if filename is not None:
+        save_df.to_csv(path.join(RESULTS_PATH, 'analysis', f'starts_variables_{filename}.csv'))
+        save_df['start_location'] = save_df['start_location'].apply(wkt.loads)
+        GeoDataFrame(save_df, geometry='start_location', crs=4326).to_file(
+            path.join(RESULTS_PATH, 'geo_data', 'analysis', f'starts_variables_{filename}.gpkg'))
+    else:
+        return save_df
+
+
 # calculate all variables and write to new files
 # {args} timesdate: str, count: int, level: str
 # {returns} None, writes new files
@@ -73,6 +141,8 @@ def get_all_variables(timesdate, count, level):
 
         write_new_columns(df, times, filename)
         write_point_layer(df, times, filename, count)
+        get_variables_per_time(df, times, filename)
+        get_variables_per_start(times, filename)
         print(f'{area} done!')
 
 
